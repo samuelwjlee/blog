abe795beb3b0f4701c2a9b5d305ee84c48f974d3
samlee <samuelwjlee@gmail.com>
2020-11-17 11:14:28 -0800

    Move __tests__ back into src

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-import App from '../src/App';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
