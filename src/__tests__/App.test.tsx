b10b52f76c8a4a5e107d3bde2ddf6a0e1dddd0db
samlee <samuelwjlee@gmail.com>
2020-11-17 16:38:41 -0800

    Remove css and downgrade react-script

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import App from 'client/App';
+import App from 'client/components/App';
 
 test('App render', () => {
   render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'client/index.css';
-import App from 'client/App';
+import App from 'client/components/App';
 import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
