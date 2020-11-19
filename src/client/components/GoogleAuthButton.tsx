abfaf0c4f07d1ef2fd25e834956916d362d262c7
samlee <samuelwjlee@gmail.com>
2020-11-19 09:02:31 -0800

    Add log in and logout func to google Auth

--- src/client/components/GoogleAuthButton.tsx
+++ src/client/components/GoogleAuthButton.tsx
@@ -1,40 +1,8 @@
-import React, { useEffect } from 'react';
-
-type GoogleUser = {
-  getBasicProfile: () => {
-    getId: () => string,
-    getName: () => string,
-    getImageUrl: () => string,
-    getEmail: () => string
-  }
-}
-
-const onSuccess = (googleUser: GoogleUser) => {
-  const profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-const onFailure = (error: any) => console.log(error);
-
-const renderButton = () => {
-  console.log('rendering');
-  if (window && (window as any).gapi) {
-    (window as any).gapi.signin2.render('g-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
-  }
-}
+import React, { useEffect, useState } from 'react';
 
 const GoogleAuthButton: React.FC = () => {
+  const [ userInfo, setuserInfo ] = useState({ name: '', email: '', profileImageUrl: '' });
+
   /* Appened required Google meta and script tags to render auth button on mount */
   useEffect(() => {
     const googleAuthMetaTag = document.createElement('meta');
@@ -42,7 +10,7 @@ const GoogleAuthButton: React.FC = () => {
 
     googleAuthMetaTag.name = 'google-signin-client_id';
     googleAuthMetaTag.content = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID || '';
-    googleScriptTag.src = `https://apis.google.com/js/platform.js?onload=${renderButton}`;
+    googleScriptTag.src = 'https://apis.google.com/js/platform.js';
     googleScriptTag.async = true;
     googleScriptTag.defer = true;
 
@@ -50,8 +18,81 @@ const GoogleAuthButton: React.FC = () => {
     document.body.appendChild(googleScriptTag);
   }, [])
 
+  const signIn = (e: any) => {
+    if (e) {
+      e.preventDefault() // to prevent submit if used within form
+    }
+    if (window && (window as any).gapi) {
+      const auth2 = (window as any).gapi.auth2.getAuthInstance()
+      // const { onLoginSuccess, onLoginFailure, onRequest } = this.props
+
+      // onRequest();
+
+      auth2.signIn()
+        .then(
+          (res: any) => {
+            const profile = res.getBasicProfile();
+
+            setuserInfo({
+              name: profile.getEmail(),
+              email: profile.getName(),
+              profileImageUrl: profile.getImageUrl(),
+            })
+          },
+          (err: any) => console.log(err)
+        );
+    }
+  }
+
+  const signOut = (e: any) => {
+    if (e) {
+      e.preventDefault() // to prevent submit if used within form
+    }
+    if (
+      window && (window as any).gapi &&
+      (window as any).gapi.auth2.getAuthInstance().isSignedIn.get()
+      ) {
+      const auth2 = (window as any).gapi.auth2.getAuthInstance()
+      // const { onLoginSuccess, onLoginFailure, onRequest } = this.props
+
+      // onRequest();
+
+      auth2.signOut()
+        .then(
+          (res: any) => {
+            setuserInfo({
+              name: '',
+              email: '',
+              profileImageUrl: ''
+            })
+          },
+          (err: any) => console.log(err)
+        );
+    }
+  }
+
   return (
-    <div className='g-signin2' />
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {
+          userInfo.profileImageUrl &&
+          <img style={{ width: '50%' }} src={userInfo.profileImageUrl} />
+        }
+        {
+          userInfo.name &&
+          `Hi there, ${userInfo.name}!`
+        }
+        <br/>
+        {
+          userInfo.email &&
+          `Your Email: ${userInfo.email}`
+        }
+      </div>
+      <div style={{ display: 'flex' }}>
+        <div className="g-signin2" onClick={signIn}></div>
+        <button onClick={signOut}>Log out</button>
+      </div>
+    </>
   );
 };
 
