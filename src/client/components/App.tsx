a71e5b4f07412d6e9ff7b95a35cb68ff23856566
samlee <samuelwjlee@gmail.com>
2020-11-22 12:18:05 -0800

    Add react-jss

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss'
+
 import AuthSection from 'client/components/AuthSection';
 
+const useStyles = createUseStyles({
+  "@global": {
+    body: {
+      fontFamily: 'Open Sans, sans-serif',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      margin: 0,
+      display: 'flex',
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: 'column',
+      width: '100vw',
+      height: '100vh'
+    }
+  }
+});
+
 const App: React.FC = () => {
+  useStyles();
+
   return (
     <div className='App'>
       <AuthSection />
