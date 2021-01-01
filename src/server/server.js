b3021fb9685034dc89c9d9cb3f0161d928bc84de
samlee <samuelwjlee@gmail.com>
2021-01-01 08:23:22 -0800

    Move sql file to root

--- src/server/server.js
+++ src/server/server.js
@@ -29,6 +29,7 @@ app.use('/users', userRouter)
  * 1. split client and server code
  * 2. deploy to heroku
  * https://www.taniarascia.com/node-express-postgresql-heroku/#deploy-app-to-heroku
+ * 3. create procfile
  */
 app.listen(process.env.EXPRESS_SERVER_PORT, () => {
   console.log(`Server listening on port: ${process.env.EXPRESS_SERVER_PORT}`)
