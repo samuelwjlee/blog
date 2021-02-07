57c16553a042bfab354eb2f22171211bd88c9335
samlee <samuelwjlee@gmail.com>
2021-02-07 10:31:03 -0800

    Update DB_URL in config

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const pool = new Pool({
   user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
